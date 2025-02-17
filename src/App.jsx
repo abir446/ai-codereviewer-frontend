2ef0dd7ed8b05e6967fd8d235354e68148d6471a
Abir Dutta <abirdutta390@gmail.com>
2025-02-17 14:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from "prismjs";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Markdown from "react-markdown";
+
+function App() {
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 2;
+}`);
+
+  const [review, setReview] = useState("");
+  useEffect(() => {
+    prism.highlightAll();
+  });
+
+  const codeReview = async () => {
+    const response = await axios.post("http://localhost:8080/ai/get-review", {
+      code,
+    });
+    console.log(response.data);
+    setReview(response.data);
+  };
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              style={{
+                height: "100%",
+                width: "100%",
+                fontSize: 16,
+              }}
+            />
+          </div>
+          <div onClick={codeReview} className="review">
+            Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
